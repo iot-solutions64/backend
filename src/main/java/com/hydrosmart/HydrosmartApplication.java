8ec2c9a839cda75c39766ab9e46af3d2556e2c0b
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2025-05-09 19:54:42 -0500

    first commit

---
+++ src/main/java/com/hydrosmart/HydrosmartApplication.java
@@ -0,0 +1,13 @@
+package com.hydrosmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydrosmartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HydrosmartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hydrosmart/HydrosmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hydrosmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydrosmartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
