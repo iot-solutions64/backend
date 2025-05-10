229ff2657ac8e0c01e96af2e97fde729a853b5ce
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2025-05-09 20:50:25 -0500

    feat(security): added infrastructure layer

--- src/main/java/com/hydrosmart/security/infrastructure/hashing/bcrypt/BCryptHashingService.java
+++ src/main/java/com/hydrosmart/security/infrastructure/hashing/bcrypt/BCryptHashingService.java
@@ -1,4 +1,12 @@
 package com.hydrosmart.security.infrastructure.hashing.bcrypt;
 
-public interface BCryptHashingService {
+import com.hydrosmart.security.application.internal.outboundservices.hashing.HashingService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * This interface is a marker interface for the BCrypt hashing service.
+ * It extends the {@link HashingService} and {@link PasswordEncoder} interfaces.
+ * This interface is used to inject the BCrypt hashing service in the {@link HashingService} class.
+ */
+public interface BCryptHashingService extends HashingService, PasswordEncoder {
 }
