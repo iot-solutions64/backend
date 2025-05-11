ad8554116dde3218b6ed1f3ee35e7a4280cf282e
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2025-05-10 21:24:09 -0500

    refactor(soil): changed the order of the parameters

--- src/main/java/com/hydrosmart/soil/domain/model/commands/CreateCropCommand.java
+++ src/main/java/com/hydrosmart/soil/domain/model/commands/CreateCropCommand.java
@@ -3,7 +3,7 @@ package com.hydrosmart.soil.domain.model.commands;
 import jakarta.validation.constraints.NotBlank;
 
 public record CreateCropCommand(
+        @NotBlank Long userId,
         @NotBlank Long temperatureId,
-        @NotBlank Long humidityId,
-        @NotBlank Long userId
+        @NotBlank Long humidityId
 ) {}
