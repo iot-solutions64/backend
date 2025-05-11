b93b4be50758e64d4a16454ebd7ec95aadb27e79
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2025-05-11 17:57:21 -0500

    fix(soil): added missing @service

--- src/main/java/com/hydrosmart/soil/domain/services/queryservices/HumidityQueryService.java
+++ src/main/java/com/hydrosmart/soil/domain/services/queryservices/HumidityQueryService.java
@@ -2,9 +2,11 @@ package com.hydrosmart.soil.domain.services.queryservices;
 
 import com.hydrosmart.soil.domain.model.entities.Humidity;
 import com.hydrosmart.soil.domain.model.queries.GetHumidityByIdQuery;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public interface HumidityQueryService {
     Optional<Humidity> handle(GetHumidityByIdQuery query);
 }
--- src/main/java/com/hydrosmart/soil/domain/services/queryservices/TemperatureQueryService.java
+++ src/main/java/com/hydrosmart/soil/domain/services/queryservices/TemperatureQueryService.java
@@ -2,9 +2,11 @@ package com.hydrosmart.soil.domain.services.queryservices;
 
 import com.hydrosmart.soil.domain.model.entities.Temperature;
 import com.hydrosmart.soil.domain.model.queries.GetTemperatureByIdQuery;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public interface TemperatureQueryService {
     Optional<Temperature> handle(GetTemperatureByIdQuery query);
 }
