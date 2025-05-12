d74383682c8664c7156d703c45f4a89dcb79c8b8
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2025-05-12 00:54:28 -0500

    fix(soil): added missing inheritance

--- src/main/java/com/hydrosmart/soil/infrastructure/persistence/jpa/repositories/TemperatureStatusRepository.java
+++ src/main/java/com/hydrosmart/soil/infrastructure/persistence/jpa/repositories/TemperatureStatusRepository.java
@@ -2,12 +2,13 @@ package com.hydrosmart.soil.infrastructure.persistence.jpa.repositories;
 
 import com.hydrosmart.soil.domain.model.entities.TemperatureStatus;
 import com.hydrosmart.soil.domain.model.valueobjects.TemperatureStatusList;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface TemperatureStatusRepository {
+public interface TemperatureStatusRepository extends JpaRepository<TemperatureStatus, Long> {
     Optional<TemperatureStatus> findByName(TemperatureStatusList status);
     boolean existsByName(TemperatureStatusList status);
 }
