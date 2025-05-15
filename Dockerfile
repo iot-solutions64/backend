722a6f305404287baa0a7b07343d6c9e2309e4f2
NickDavid1811 <u20201c607@upc.edu.pe>
2025-05-14 22:21:12 -0500

    chore: set up Docker environment with env variables

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM maven:3.9-eclipse-temurin-24 AS builder
+WORKDIR /app
+COPY . .
+RUN mvn clean package -DskipTests
+
+FROM eclipse-temurin:24-jdk
+WORKDIR /app
+
+COPY --from=builder /app/target/*.jar app.jar
+
+ENV PORT=8080
+EXPOSE ${PORT}
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/hydrosmart/HydrosmartApplication.java
+++ src/main/java/com/hydrosmart/HydrosmartApplication.java
@@ -2,7 +2,11 @@ package com.hydrosmart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -12,4 +16,19 @@ public class HydrosmartApplication {
         SpringApplication.run(HydrosmartApplication.class, args);
     }
 
+    @Configuration
+    public static class MyConfiguration {
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedOrigins("*")
+                            .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH")
+                            .allowedHeaders("*");
+                }
+            };
+        }
+    }
 }
