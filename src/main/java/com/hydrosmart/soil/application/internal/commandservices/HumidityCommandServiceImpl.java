2c9395faa92605dba49cebfce8ddbbb49a1c4037
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2025-05-17 21:37:17 -0500

    chore: made changes to allow merging

--- src/main/java/com/hydrosmart/soil/application/internal/commandservices/HumidityCommandServiceImpl.java
+++ src/main/java/com/hydrosmart/soil/application/internal/commandservices/HumidityCommandServiceImpl.java
@@ -17,40 +17,31 @@ import java.util.Optional;
 public class HumidityCommandServiceImpl implements HumidityCommandService {
     private final HumidityRepository humidityRepository;
     private final HumidityStatusRepository humidityStatusRepository;
-
-    public HumidityCommandServiceImpl(HumidityRepository humidityRepository,
-            HumidityStatusRepository humidityStatusRepository) {
+    public HumidityCommandServiceImpl(HumidityRepository humidityRepository, HumidityStatusRepository humidityStatusRepository){
         this.humidityRepository = humidityRepository;
         this.humidityStatusRepository = humidityStatusRepository;
     }
 
-    private void validateHumidity(Float min, Float max) {
-        if (min > max)
-            throw new RuntimeException("The min threshold cannot be greater than the max threshold");
+    private void validateHumidity(Float min, Float max){
+        if(min > max) throw new RuntimeException("The min threshold cannot be greater than the max threshold");
     }
 
-    private HumidityStatus findStatus(HumidityStatusList status) {
+    private HumidityStatus findStatus(HumidityStatusList status){
         return humidityStatusRepository.findByName(status)
                 .orElseThrow(() -> new RuntimeException("Status not found"));
     }
 
-    private HumidityStatus validateHumidityStatus(Float humidity, Float min, Float max) {
-        if (humidity >= min && humidity <= max)
-            return findStatus(HumidityStatusList.FAVORABLE);
-        if (humidity > max && humidity < max + 5)
-            return findStatus(HumidityStatusList.SLIGHTLY_UNFAVORABLE_OVER);
-        if (humidity < min && humidity > min - 5)
-            return findStatus(HumidityStatusList.SLIGHTLY_UNFAVORABLE_UNDER);
-        if (humidity > max + 5 && humidity < max - 10)
-            return findStatus(HumidityStatusList.UNFAVORABLE_OVER);
-        if (humidity < min + 5 && humidity > min - 10)
-            return findStatus(HumidityStatusList.UNFAVORABLE_UNDER);
-        if (humidity > max + 10)
-            return findStatus(HumidityStatusList.FLOODED);
+    private HumidityStatus validateHumidityStatus(Float humidity, Float min, Float max){
+        if(humidity >= min && humidity <= max) return findStatus(HumidityStatusList.FAVORABLE);
+        if(humidity > max && humidity <= max + 5) return findStatus(HumidityStatusList.SLIGHTLY_UNFAVORABLE_OVER);
+        if(humidity < min && humidity >= min - 5) return findStatus(HumidityStatusList.SLIGHTLY_UNFAVORABLE_UNDER);
+        if(humidity > max + 5 && humidity <= max + 10) return findStatus(HumidityStatusList.UNFAVORABLE_OVER);
+        if(humidity < min - 5 && humidity >= min - 10) return findStatus(HumidityStatusList.UNFAVORABLE_UNDER);
+        if(humidity > max + 10) return findStatus(HumidityStatusList.FLOODED);
         return findStatus(HumidityStatusList.DRY);
     }
 
-    private Humidity findHumidity(Long id) {
+    private Humidity findHumidity(Long id){
         return humidityRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Humidity not found"));
     }
@@ -59,7 +50,7 @@ public class HumidityCommandServiceImpl implements HumidityCommandService {
     public Optional<Humidity> handle(CreateHumidityCommand command) {
         validateHumidity(command.humidityMinThreshold(), command.humidityMaxThreshold());
         var status = findStatus(HumidityStatusList.FAVORABLE);
-        var humidity = new Humidity(command, status);
+        var humidity = new Humidity(command,status);
         humidityRepository.save(humidity);
         return Optional.of(humidity);
     }
@@ -68,8 +59,7 @@ public class HumidityCommandServiceImpl implements HumidityCommandService {
     public Optional<Humidity> handle(UpdateHumidityCommand command) {
         var foundHumidity = findHumidity(command.id());
         validateHumidity(command.humidityMinThreshold(), command.humidityMaxThreshold());
-        var status = validateHumidityStatus(command.humidity(), command.humidityMinThreshold(),
-                command.humidityMaxThreshold());
+        var status = validateHumidityStatus(command.humidity(), command.humidityMinThreshold(), command.humidityMaxThreshold());
         var updatedHumidity = humidityRepository.save(foundHumidity.updateHumidity(command, status));
         return Optional.of(updatedHumidity);
     }
@@ -78,8 +68,7 @@ public class HumidityCommandServiceImpl implements HumidityCommandService {
     public void handle(PatchHumidityCommand command) {
         var foundHumidity = findHumidity(command.id());
         validateHumidity(foundHumidity.getHumidityMinThreshold(), foundHumidity.getHumidityMaxThreshold());
-        var status = validateHumidityStatus(command.humidity(), foundHumidity.getHumidityMinThreshold(),
-                foundHumidity.getHumidityMaxThreshold());
+        var status = validateHumidityStatus(command.humidity(), foundHumidity.getHumidityMinThreshold(), foundHumidity.getHumidityMaxThreshold());
         humidityRepository.save(foundHumidity.patchHumidity(command, status));
     }
 }
