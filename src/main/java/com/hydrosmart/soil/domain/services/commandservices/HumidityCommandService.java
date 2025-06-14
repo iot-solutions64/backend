2b780baebc881a9a66fc958702e9acdf5f73e7a7
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2025-06-14 11:36:38 -0500

    hotfix: changed the way the endpoint data is handled

--- src/main/java/com/hydrosmart/soil/domain/services/commandservices/HumidityCommandService.java
+++ src/main/java/com/hydrosmart/soil/domain/services/commandservices/HumidityCommandService.java
@@ -2,13 +2,13 @@ package com.hydrosmart.soil.domain.services.commandservices;
 
 import com.hydrosmart.soil.domain.model.commands.CreateHumidityCommand;
 import com.hydrosmart.soil.domain.model.commands.PatchHumidityCommand;
-import com.hydrosmart.soil.domain.model.commands.UpdateHumidityCommand;
+import com.hydrosmart.soil.domain.model.commands.PatchHumidityThresholdCommand;
 import com.hydrosmart.soil.domain.model.entities.Humidity;
 
 import java.util.Optional;
 
 public interface HumidityCommandService {
     Optional<Humidity> handle(CreateHumidityCommand command);
-    Optional<Humidity> handle(UpdateHumidityCommand command);
+    Optional<Humidity> handle(PatchHumidityThresholdCommand command);
     void handle(PatchHumidityCommand command);
 }
--- src/main/java/com/hydrosmart/soil/domain/services/commandservices/TemperatureCommandService.java
+++ src/main/java/com/hydrosmart/soil/domain/services/commandservices/TemperatureCommandService.java
@@ -2,13 +2,13 @@ package com.hydrosmart.soil.domain.services.commandservices;
 
 import com.hydrosmart.soil.domain.model.commands.CreateTemperatureCommand;
 import com.hydrosmart.soil.domain.model.commands.PatchTemperatureCommand;
-import com.hydrosmart.soil.domain.model.commands.UpdateTemperatureCommand;
+import com.hydrosmart.soil.domain.model.commands.PatchTemperatureThresholdCommand;
 import com.hydrosmart.soil.domain.model.entities.Temperature;
 
 import java.util.Optional;
 
 public interface TemperatureCommandService {
     Optional<Temperature> handle(CreateTemperatureCommand command);
-    Optional<Temperature> handle(UpdateTemperatureCommand command);
+    Optional<Temperature> handle(PatchTemperatureThresholdCommand command);
     void handle(PatchTemperatureCommand command);
 }
